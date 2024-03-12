62c42fc8ea705388ad703d814e4ba16f8288bd79
ChrisTR1021 <christianridgell1021@gmail.com>
2024-03-12 01:02:45 -0400

    added case, break and const for departments

--- index.js
+++ index.js
@@ -37,5 +37,41 @@ const init = () => {
                     "Exit "
                 ]
             }
-        ])
+        ]).then(ans => {
+            //ans. initialize
+        switch (ans.initialize) {
+            case "View all departments": viewDepartment();
+                break;
+            case "View all roles": viewRoles();
+                break;
+            case "View all employees": viewEmployees();
+                break;
+            case "Add a department": addDepartment();
+                break;
+            case "Add a role": addRoles();
+                break;
+            case "Add an employee": addEmployee();
+                break;
+            case "Update an employee role": updateEmployee();
+                break;
+            case "Exit":
+                console.log("Thank you very much!");
+                process.exit();
+        }
+    }).catch(err => console.error(err));
+
+    }
+
+    init();
+
+    const viewDepartment = () => {
+        db.query(`SELECT * FROM department`, (err, results) => {
+            err? console.error(err) : console.table(results);
+            init();
+        })
+    };
+
+    
+        }
+            )
 }
